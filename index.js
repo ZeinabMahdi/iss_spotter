7990c06a62f1ec19370ed513df2e07d629556d4f
Zeinab Mahdi <zeinab_750@hotmail.com>
2021-12-02 20:59:00 -0500

    commit

--- index.js
+++ index.js
@@ -29,10 +29,19 @@ const {nextISSTimesForMyLocation} = require('./iss');
 //   console.log("It works! Returned flyover times:", times);
 // });
 
-nextISSTimesForMyLocation((error, passTimes) => {
+const printedTimes = function(times) {
+  for (const pass of times) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, times) => {
   if (error) {
     return console.log("It didn't work!", error);
+  } else {
+    printedTimes(times);
   }
-  // success, print out the deets!
-  console.log(passTimes);
 });
--- iss.js
+++ iss.js
@@ -61,7 +61,22 @@ const fetchISSFlyOverTimes = function(coords, callback) {
 };
 
 const nextISSTimesForMyLocation = function(callback) {
-
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+    fetchCoordsByIP(ip, (error, coords) => {
+      if (error) {
+        return callback(error, null);
+      }
+      fetchISSFlyOverTimes(coords, (error, nextPasses) => {
+        if (error) {
+          return callback(error, null);
+        }
+        callback(null, nextPasses);
+      });
+    });
+  });
 };
 module.exports = { nextISSTimesForMyLocation };
 
